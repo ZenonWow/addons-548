00e5ee42150d7037192846f84d6125b323ed4e72
ZenonWow <ZenonWow@mail.com>
2018-09-25 23:22:22 +0200

    Broker_Everything / stuff.lua: fullscreen saves and restores windowed state
    Shift+Alt-LeftClick - Toggle Window/Fullscreen mode
    Shift+Ctrl-LeftClick - Reload UI
    Shift+Control-RightClick - Logout
    Shift+Control+Alt-RightClick - Quit game

--- Broker_Everything/modules/stuff.lua
+++ Broker_Everything/modules/stuff.lua
@@ -13,7 +13,6 @@ local name = "Stuff" -- L["Stuff"]
 local ldbName = name
 local ttName = name.."TT"
 local tt = nil
-local last_click = 0
 
 
 -- ------------------------------------- --
@@ -70,6 +69,36 @@ end
 
 --[[ ns.modules[name].onmousewheel = function(self,direction) end ]]
 
+if  GetCVarBool == nil  then
+	function GetCVarBool(cvar)  return GetCVar(cvar) == "1"  end
+end
+
+--[[
+/run SetCVar("gxMaximize", 1); SetCVar("gxWindow", 1); RestartGx()
+--]]
+function ToggleFullscreen() 
+  local wasMaximized= GetCVarBool("gxMaximize")
+  local wasWindowed= GetCVarBool("gxWindow")
+  local wasFullscreen= wasMaximized or not wasWindowed
+  Broker_EverythingDB.Stuff= Broker_EverythingDB.Stuff or {}
+  if  wasFullscreen  then
+    -- save windowed state of fullscreen before switching
+    Broker_EverythingDB.Stuff.fullscreenWindowed= wasWindowed
+    -- go windowed and not maximized
+    ns.SetCVar("gxMaximize", 0)
+    ns.SetCVar("gxWindow", 1)
+  else
+    local fullscreenIsWindowed= Broker_EverythingDB.Stuff.fullscreenIsWindowed
+    if  fullscreenIsWindowed == nil  then  fullscreenIsWindowed= true  end
+		local varValue= fullscreenIsWindowed and 1 or 0
+    ns.SetCVar("gxMaximize", varValue)  -- awkward logic: non-windowed fullscreen is not maximized
+    --if  fullscreenIsWindowed  then  ns.SetCVar("gxMaximize", 1)  end
+    ns.SetCVar("gxWindow", varValue)
+  end
+  --ns.SetCVar("gxWindow", 1 - tonumber(GetCVar("gxWindow")))
+  RestartGx()
+end
+
 ns.modules[name].ontooltip = function(tt)
 	if (not tt.key) or tt.key~=ttName then return end -- don't override other LibQTip tooltips...
 
@@ -79,26 +108,32 @@ ns.modules[name].ontooltip = function(tt)
 	tt:AddHeader(C("dkyellow",L[name])) 
 	tt:AddLine (" ")
 	
-	line, column = tt:AddLine(L["Windowed / Fullscreen"])
-	tt:SetLineScript(line, "OnMouseUp", function(self) 
-		ns.SetCVar("gxWindow", 1 - tonumber(GetCVar("gxWindow")))
-		RestartGx()
+	line, column = tt:AddLine(L["Windowed / Fullscreen (press Alt)"])
+	tt:SetLineScript(line, "OnMouseUp", function(self)
+		if  not IsControlKeyDown()  and  IsAltKeyDown()  then  ToggleFullscreen()  end
+	end )
+	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
+	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
+	
+	line, column = tt:AddLine(L["Reload UI (press Control)"])
+	tt:SetLineScript(line, "OnMouseUp", function(self)
+		if  IsControlKeyDown()  and  not IsAltKeyDown()  then  securecall("ReloadUI")  end
+		if  not IsControlKeyDown()  and  not IsAltKeyDown()  then  StaticPopup_Show("CONFIRM")  end  -- Use StaticPopup to avoid taint.
+	end )
+	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
+	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
+	
+	line, column = tt:AddLine(L["Logout (Control-RightClick)"])
+	tt:SetLineScript(line, "OnMouseUp", function(self,button)
+		if  button == "RightButton"  and  IsControlKeyDown()  and  not IsAltKeyDown()  then  securecall("Logout")  end
 	end)			
 	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
 	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
 	
-	line, column = tt:AddLine(L["Reload UI"])
-	tt:SetLineScript(line, "OnMouseUp", function(self)	StaticPopup_Show("CONFIRM")	end) -- Use static Popup to avoid taint.
-	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
-	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
-	
-	line, column = tt:AddLine(L["Logout"])
-	tt:SetLineScript(line, "OnMouseUp", function(self) Logout() end)			
-	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
-	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
-	
-	line, column = tt:AddLine(L["Quit Game"])
-	tt:SetLineScript(line, "OnMouseUp", function(self) Quit() end)			
+	line, column = tt:AddLine(L["Quit Game (Control+Alt-RightClick)"])
+	tt:SetLineScript(line, "OnMouseUp", function(self,button)
+		if  button == "RightButton"  and  IsControlKeyDown()  and  IsAltKeyDown()  then  securecall("Quit")  end
+	end)			
 	tt:SetLineScript(line, "OnEnter", function(self) tt:SetLineColor(line, 1,192/255, 90/255, 0.3) end )
 	tt:SetLineScript(line, "OnLeave", function(self) tt:SetLineColor(line, 0,0,0,0) end)
 	
@@ -106,13 +141,13 @@ ns.modules[name].ontooltip = function(tt)
 		tt:AddLine(" ")
 		line, column = nil, nil
 		tt:AddLine(
-			C("copper",L["Left-click"]).." || "..C("green",L["Logout"])
+			C("copper",L["Shift+Alt-LeftClick"]).." || "..C("green",L["Toggle Window/Fullscreen mode"])
 			.."|n"..
-			C("copper",L["Right-click"]).." || "..C("green",L["Quit game"])
---			.."|n"..
---			C("copper",L["Shift+Left-click"]).." || "..C("green",L["Switch window/fullscreen mode"])
+			C("copper",L["Shift+Ctrl-LeftClick"]).." || "..C("green",L["Reload UI"])
 			.."|n"..
-			C("copper",L["Shift+Left-click"]).." || "..C("green",L["Reload UI"])
+			C("copper",L["Shift+Control-RightClick"]).." || "..C("green",L["Logout"])
+			.."|n"..
+			C("copper",L["Shift+Control+Alt-RightClick"]).." || "..C("green",L["Quit game"])
 		)
 	end
 end
@@ -134,36 +169,12 @@ ns.modules[name].onleave = function(self)
 end
 
 ns.modules[name].onclick = function(self,button)
-	local shift = IsShiftKeyDown() or -1
-	local cv = 0
-
-	if button == "RightButton" then
-		cv = 1 * shift
-	elseif button == "LeftButton" then
-		cv = 2 * shift
-	end
-
-	if last_click ~= cv then
-		last_click = cv
-		return
-	end
-
-	-- only for windowed mode switching...
-	last_click = 0
-
-	if shift > 0 then
-		if button == "LeftButton" then
-			securecall("ReloadUI")
-		elseif button == "RightButton" then
-			--SetCVar("gxWindow", 1 - GetCVar("gxWindow"))
-			--RestartGx()
-		end
-	else
-		if button == "LeftButton" then
-			securecall("Logout")
-		elseif button == "RightButton" then
-			securecall("Quit")
-		end
+	if  IsShiftKeyDown()  and  button == "LeftButton"  then
+		if  not IsControlKeyDown()  and  IsAltKeyDown()  then  ToggleFullscreen()  end
+		if  IsControlKeyDown()  and  not IsAltKeyDown()  then  securecall("ReloadUI")  end
+	elseif  IsShiftKeyDown()  and  button == "RightButton"  then
+		if  IsControlKeyDown()  and  not IsAltKeyDown()  then  securecall("Logout")  end
+		if  IsControlKeyDown()  and  IsAltKeyDown()  then  securecall("Quit")  end
 	end
 end
 
