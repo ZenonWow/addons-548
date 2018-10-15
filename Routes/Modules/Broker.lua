eb5bd7db7a04bce71e2718a1bda02e36612b63c0
ZenonWow <ZenonWow@mail.com>
2018-10-15 17:37:56 +0200

    Routes: added icon and basic launcher from Broker_Routes

---
+++ Routes/Modules/Broker.lua
@@ -0,0 +1,9 @@
+LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Routes", {
+	type = "launcher",
+	label = "Routes",
+	icon = "Interface\\Addons\\Routes\\icon.tga",
+	OnClick = function(clickedframe, button)
+		--Routes.ToggleConfig()
+		LibStub("AceAddon-3.0"):GetAddon("Routes").ToggleConfig()
+	end,
+});
