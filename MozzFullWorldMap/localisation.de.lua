0670814b64468d3af867c5ce3d43eafeda2e7950
ZenonWow <ZenonWow@mail.com>
2018-10-25 18:21:56 +0200

    Delete localizations

--- MozzFullWorldMap/localisation.de.lua
+++
@@ -1,6 +0,0 @@
-
-if ( GetLocale() == "deDE" ) then
-
-	MFWM.L["BINDING_NAME_MOZZ_WM_CHECKBOX"] = "Zeigen Sie die noch unbekannten Sektoren";
-	
-end
--- MozzFullWorldMap/localisation.tw.lua
+++
@@ -1,5 +0,0 @@
-if (GetLocale() == "zhTW") then
-
-	MFWM.L["BINDING_NAME_MOZZ_WM_CHECKBOX"]	= "顯示未探索的區域";
-
-end
