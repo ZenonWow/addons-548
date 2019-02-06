b7095adf3b9fed5d6f0c0608ece583ef218b86cb
ZenonWow <ZenonWow@mail.com>
2019-02-06 01:15:09 +0100

    CloseOneWindow(): fix Game Menu not opening for ESC key -- a usually avoided bug in builtin CloseAllWindows() -- by calling HideUIPanel(frame) instead of frame:Hide()
    Bug encountered when ScriptErrorsFrame is elevated to UIPanel and closed with :Hide().
    *.toc: disable some unused scracth code
    DominosSwap.lua: use LibStubs.AceEvent30
    LibCloseOneWindow: Avoid BUG in FrameXML/UIParent.lua#CloseWindows(): finding a UIPanel to close after it's been :Hide() -n by CloseSpecialWindows() and making the Game Menu unreachable with ESC key.
    Make frames nicely print their name for tostring(frame).

--- +dev/ZenTools/LibCloseOneWindow.lua
+++ +dev/ZenTools/LibCloseOneWindow.lua
@@ -10,6 +10,10 @@ function CloseOneWindow()
 		-- frame:GetLeft() is a simple on-screen check. Returns nil if the frame was not positioned. If it was, then it's most probably on-screen.
 		if  frame  and  frame:IsVisible()  then
 			print("ESC -> CloseOneWindow(): UISpecialFrames["..index.."] == "..frameName)
+			-- Avoid BUG in  FrameXML/UIParent.lua#CloseWindows():  finding a UIPanel to close after it's been :Hide() -n by CloseSpecialWindows() and making the Game Menu unreachable with ESC key.
+			-- Source of the bug:  FramePositionDelegate:HideUIPanel() not being called when :Hide() is called instead of HideUIPanel()
+			-- Next source of the bug:  GetUIPanel(f) returning a hidden frame, assuming :Hide() is not called directly.
+			-- Usually this bug does not surface as UIPanels are not listed in UISpecialFrames.
 			HideUIPanel(frame)
 			if  frame:GetLeft()  then  return frame  end
 		end
@@ -17,8 +21,25 @@ function CloseOneWindow()
 	-- print("ESC -> CloseOneWindow(): nothing to close.")
 end
 
--- Return count of bags closed or nil if none opened
-function CloseAllBags2()
+--[[
+/dump getmetatable(RaidBrowserFrame).__index.Hide
+Demonstrate bug:
+/run local r,g=RaidBrowserFrame,GetUIPanel;getmetatable(r).__tostring=function(f) return f:GetName() end; ShowUIPanel(r);r:Hide(); print("Press ESC, Game Menu won't show. GetUIPanel()s:",g('left'),g('center'),g('right'),g('doublewide'),g('fullscreen'))
+To Regain Game Menu run this as many times as the previous (max 3 times necessary):
+/run RaidBrowserFrame:Show() ; HideUIPanel(RaidBrowserFrame) ; print("Press ESC, Game Menu WILL show.")
+Proper fix: hook OnHide on UIPanel frames and call FramePositionDelegate:HideUIPanel(frame) ; FramePositionDelegate is a private local in UIParent.lua, not reachable by addon code.
+Patch 1:  hook OnHide, call f:Show() and HideUIPanel(f)
+Patch 2:  hook OnHide, set f.IsShown=function() return true end ; call HideUIPanel(f) ; reset f.IsShown
+Patch 3:  replace CloseSpecialWindows() with a fixed one using HideUIPanel()
+Patch 4:  hook CloseWindows(...)  local f=CloseWindows_orig(...) ; return not f:IsShown() and f  end
+Patch 5:  hook GetUIPanel(...)  local f=GetUIPanel_orig(...) ; return f and f:IsShown() and f  end
+--]]
+
+-- Reimplementation of FrameXML/ContainerFrame.lua#CloseAllBags() with O(n) instead of O(n^2) operations (n == NUM_CONTAINER_FRAMES == 13).
+-- Additionally it returns count of bags closed or nil if none was open.
+function CloseAllBagsLinear(requestingFrame)
+	if requestingFrame and requestingFrame:GetName() ~= _G.FRAME_THAT_OPENED_BAGS then  return  end
+	_G.FRAME_THAT_OPENED_BAGS = nil
 	local bagsVisible = nil
 	for  i = 1, NUM_CONTAINER_FRAMES  do
 		local containerFrame = _G["ContainerFrame"..i]
@@ -27,35 +48,52 @@ function CloseAllBags2()
 			bagsVisible = (bagsVisible or 0) + 1
 		end
 	end
-	-- print("ESC -> CloseAllBags2(): closed "..(bagsVisible or "ZERO").." container frames")
+	-- print("ESC -> CloseAllBagsLinear(): closed "..(bagsVisible or "ZERO").." container frames")
 	return bagsVisible
 end
 
-_G.CloseAllBags_ = _G.CloseAllBags
-_G.CloseAllBags = CloseAllBags2
+_G._Original = _G._Original or {}
+_G._Original.CloseAllBags = _G.CloseAllBags
+_G.CloseAllBags = CloseAllBagsLinear
 
-_G.CloseAllWindows_ = _G.CloseAllWindows
+-- Reimplementation of FrameXML/UIParent.lua#CloseAllWindows() to close just _one_ window when ESC is pressed.
+-- Original CloseAllWindows == CloseAllBagsLinear + CloseWindows
+-- Original CloseWindows == Close UI Panels + CloseSpecialWindows
+-- Original CloseSpecialWindows == Close frames listed in UISpecialFrames  -- hooked by addons to close their window (usually one at a time).
+_G._Original.CloseAllWindows = _G.CloseAllWindows
 function _G.CloseAllWindows(ignoreCenter, ...)
-	-- if  not called from ToggleGameMenu()  through securecall("CloseAllWindows")  then  return  end			-- do nothing
+	-- if  not called from ToggleGameMenu()  then  return  end			-- do nothing
+	print("  CloseAllWindows() called from:", _G.debugstack(2,3,0) )
 	if  ignoreCenter  then  return  end			-- don't close windows after PLAYER_DEAD, PLAYER_ENTERING_WORLD, PLAYER_CONTROL_LOST
-	local res =  CloseOneWindow()  or  CloseAllBags2()  or  _G.CloseAllWindows_(ignoreCenter, ...)
-	print("  CloseOneWindow/CloseAllWindows_orig() ->", res, res and LibCommon.istable(res) and res.GetName and res:GetName() )
+	local res =  CloseOneWindow()  or  CloseAllBagsLinear()  or  _G._Original.CloseAllWindows(ignoreCenter, ...)
+	print("  CloseOneWindow/_Original.CloseAllWindows() ->", res)
 	-- Table returned would be silently dropped by securecall(), returning nil...
 	return res and 1
 end
 
+-- Make frames nicely print their name for tostring(frame). Basic for debugging, shouldn't be?
+-- Any frame will do here, or an unnecessarily allocating CreateFrame(), if we want to be on the safe side.
+getmetatable(CharacterFrame).__tostring = function(f)
+  local name = f:GetName()  ;  if name then  return name  end
+  name = "Some "..f:GetObjectType()
+  if f.GetID then  name = name.." with ID="..tostring(f:GetID())  end
+  local parent = f.GetParent and f:GetParent()
+  if parent then  name = name.." with Parent: "..tostring(parent)  end
+  return name
+end
 
 --[[
 /run print( GetUIPanel("left"), GetUIPanel("center"), GetUIPanel("right"), GetUIPanel("doublewide"), GetUIPanel("fullscreen") )
+/run UIParent:EnableMouse( true )
 
 [09:46:30] ESC -> CloseOneWindow(): nothing to close.
-[09:46:30] ESC -> CloseAllBags2(): closed ZERO container frames
+[09:46:30] ESC -> CloseAllBagsLinear(): closed ZERO container frames
 [09:46:30] ESC -> call CloseSpecialWindows_orig()
 [09:46:30]   CloseSpecialWindows_orig() -> nil
 [09:46:30]   CloseAllWindows_orig() -> table: 13EB1CA0
 
 [08:22:05] ESC -> CloseOneWindow(): nothing to close.
-[08:22:05] ESC -> CloseAllBags2(): closed ZERO container frames
+[08:22:05] ESC -> CloseAllBagsLinear(): closed ZERO container frames
 [08:22:05] ESC -> CloseOneWindow(): nothing to close.
 [08:22:05] ESC -> call CloseSpecialWindows_orig()
 --]]
