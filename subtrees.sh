0e41ff7db939694cd968c9a74b67c06632aa362f
ZenonWow <ZenonWow@mail.com>
2019-03-10 19:14:59 +0100

    Submodules mean trouble.

---
+++ subtrees.sh
@@ -0,0 +1,3 @@
+git subtree add  -P bars/Masque   ../src/548/bars/Masque   mop-v548
+git subtree add  -P map/Squeenix  ../src/548/map/Squeenix  mop-v548
+git subtree add  -P ui/Aurora     ../src/548/ui/Aurora     mop-v548
